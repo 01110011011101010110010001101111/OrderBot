233ff7df58d6292d0e3dd60fa1085bbc4dce64a0
GenericP3rson <genericp3rson@gmail.com>
2023-11-08 23:08:23 -0500

    more or less set up simulation

--- prep/experimental_simulation.py
+++ prep/experimental_simulation.py
@@ -31,103 +31,8 @@ from manipulation.station import MakeHardwareStation, load_scenario
 meshcat = StartMeshcat()
 
 full_path = "/Users/paromitadatta/Desktop/64210/6.4210-Final-Project/objects/"
-
-table_top_sdf_file = full_path + "table_top.sdf"
-table_top_sdf = """<?xml version="1.0"?>
-<sdf version="1.7">
-
-  <model name="table_top">
-    <link name="table_top_link">
-      <inertial>
-        <mass>18.70</mass>
-        <inertia>
-          <ixx>0.79</ixx>
-          <ixy>0</ixy>
-          <ixz>0</ixz>
-          <iyy>0.53</iyy>
-          <iyz>0</iyz>
-          <izz>1.2</izz>
-        </inertia>
-      </inertial>
-    <visual name="bottom">
-        <pose>0.0 0.0 0.445 0 0 0</pose>
-        <geometry>
-          <box>
-            <size>1.49 1.63 0.015</size>
-          </box>
-        </geometry>
-        <material>
-          <diffuse>0.9 0.9 0.9 1.0</diffuse>
-        </material>
-      </visual>
-      <collision name="bottom">
-        <pose>0.0 0.0 0.445 0 0 0</pose>
-        <geometry>
-          <box>
-            <size>0.49 0.63 0.015</size>
-          </box>
-        </geometry>
-        <drake:proximity_properties>
-          <drake:compliant_hydroelastic/>
-          <drake:hydroelastic_modulus>1.0e6</drake:hydroelastic_modulus>
-        </drake:proximity_properties>
-      </collision>
-    </link>
-    <frame name="table_top_center">
-      <pose relative_to="table_top_link">0 0 0.47 0 0 0</pose>
-    </frame>
-  </model>
-</sdf>
-
-"""
-
-with open(table_top_sdf_file, "w") as f:
-    f.write(table_top_sdf)
-
-
+ 
 your_model_filename = full_path + "Pound_Cake_OBJ.sdf"
-
-if your_model_filename:
-    visualizer = ModelVisualizer(meshcat=meshcat)
-    visualizer.parser().AddModels(your_model_filename)
-    visualizer.Run(loop_once=True)
-
-scenario_data = f"""
-directives:
-- add_model:
-    name: table_top
-    file: file://{table_top_sdf_file}
-- add_weld:
-    parent: world
-    child: table_top::table_top_center
-"""
-if your_model_filename:
-    scenario_data += "directives:"
-    scenario_data += f"""
-
-- add_model:
-    name: table_top
-    file: file://{table_top_sdf_file}
-- add_weld:
-    parent: world
-    child: table_top::table_top_center
-- add_model:
-    name: your_model
-    file: file://{your_model_filename}
-    default_free_body_pose:
-        Pound_Cake_OBJ: # Change here!
-            translation: [0, 0, 0.5]
-            rotation: !Rpy {{ deg: [42, 33, 18] }}
-"""
-
-scenario = load_scenario(data=scenario_data)
-station = MakeHardwareStation(scenario, meshcat)
-
-simulator = Simulator(station)
-meshcat.StartRecording()
-simulator.AdvanceTo(2.0)
-meshcat.PublishRecording()
-
 
 scenario_data = f"""
 directives:
@@ -160,7 +65,6 @@ directives:
     X_PC:
       rotation: !Rpy {{ deg: [0.0, 0.0, 180.0 ]}}
       translation: [0.5, 0.25, -0.015]
-
 - add_model:
     name: floor
     file: package://manipulation/floor.sdf
@@ -182,28 +86,43 @@ directives:
         Pound_Cake_OBJ: # Change here!
             translation: [0, -0.6, 0.2]
             rotation: !Rpy {{ deg: [42, 33, 18] }}
+"""
+
+def add_random_obj(ranges, name, cnt):
+    global scenario_data
+    for num in range(cnt):
+        scenario_data += f"""
+- add_model:
+    name: {name}_{num}
+    file: file://{full_path}{name}.sdf
+    default_free_body_pose:
+        base_link:
+            translation: [{ranges['x'] + np.random.randint(-10, 10)/50}, {ranges['y'] + np.random.randint(-10, 10)/50}, {ranges['z'] + np.random.randint(10)/10}]
+
+""" 
+
+def add_custom_random_obj(ranges, name, cnt):
+    global scenario_data
+    for num in range(cnt):
+        scenario_data += f"""
+- add_model:
+    name: {name}_{num}
+    file: file://{full_path}{name}.sdf
+    default_free_body_pose:
+        {name}: # Change here!
+            translation: [{0.5 + np.random.randint(-10, 10)/50}, {-0.6 + np.random.randint(-10, 10)/50}, {0.01}] 
+            rotation: !Rpy {{ deg: [{np.random.randint(0, 90)}, {np.random.randint(0, 90)}, {np.random.randint(0, 90)}] }}
+"""
+   
+add_random_obj({"x": 0, "y": -0.6, "z": 0.2}, "foam_chicken", 100)
+add_custom_random_obj({"x": 0.2, "y": -0.6, "z": 0.2}, "Pound_Cake_OBJ", 25)
+
+scenario_data += f"""
 model_drivers:
     iiwa: !IiwaDriver
       hand_model_name: wsg
     wsg: !SchunkWsgDriver {{}}
 """
-
-# def add_random_obj(ranges, name, cnt):
-#     global scenario_data
-#     for num in range(cnt):
-#         scenario_data += f"""
-# 
-# directives:
-# - add_model:
-#     name: {name}_{num}
-#     file: file://{full_path}{name}.sdf
-#     default_free_body_pose:
-#         base_link:
-#             translation: [0, {-0.6 + np.random.randint(-10, 10)/10}, {0.2 + np.random.randint(-10, 10)/10}]
-# 
-# """ 
-#    
-# add_random_obj(None, "foam_chicken", 100)
 
 print(scenario_data)
 
